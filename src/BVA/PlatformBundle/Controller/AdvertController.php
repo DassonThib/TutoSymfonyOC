8f0fb7685aabfa16f58ee23f6d154859bf108c74
Thibault DASSONVILLE <thibault.dassonville@bva-group.com>
2017-11-23 18:07:06 +0100

    Ajout des templates twig

--- src/BVA/PlatformBundle/Controller/AdvertController.php
+++ src/BVA/PlatformBundle/Controller/AdvertController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
         // $content = $this
         //     ->get('templating')
@@ -35,9 +35,34 @@ class AdvertController extends Controller
         if($page < 1) throw new NotFoundHttpException('Page'.$page.' inexistante');
 
         // Ici, on récupérera la liste des annonces, puis on la passera au template
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony sur Lyon, bla bla bla',
+                'date'    => new \Datetime()
+            ),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous cherchons un webmaster capable de maintenir notre site internet, bla bla bla',
+                'date'    => new \Datetime()
+            ),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner, bla bla bla',
+                'date'    => new \Datetime()
+            )
+        );
 
         // Mais pour l'instant, on ne fait qu'appeler le template
-        return $this->render('OCPlatformBundle:Advert:index.html.twig');
+        return $this->render('BVAPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
     }
 
     // On injecte la requète dans les arguments de la méthode
@@ -78,8 +103,16 @@ class AdvertController extends Controller
         // ============================================================================
 
         // Ici on récupérera l'annonce correspondant à l'id $id
+        $advert = array(
+            'title'   => 'Recherche développeur Symfony 2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant à Lyon, Blabla',
+            'date'    => new \Datetime()
+        );
+
         return $this->render('BVAPlatformBundle:Advert:view.html.twig', array(
-            'id' => $id
+            'advert' => $advert
         ));
     }
 
@@ -128,14 +161,26 @@ class AdvertController extends Controller
     {
         // Ici on récupère l'annonce correspondant à $id
 
-        // Même mécanique que pour l'ajout
-        if($request->isMethod('POST'))
-        {
-            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
-            return $this->redirectToRoute('bva_platform_view', array('id' => $id));
-        }
+        // // Même mécanique que pour l'ajout
+        // if($request->isMethod('POST'))
+        // {
+        //     $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+        //     return $this->redirectToRoute('bva_platform_view', array('id' => $id));
+        // }
 
-        return $this->render('BVAPlatformBundle:Advert:edit.html.twig');
+        // return $this->render('BVAPlatformBundle:Advert:edit.html.twig');
+
+        $advert = array(
+            'title'   => 'Recherche développeur Symfony',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon, Blabla',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('BVAPlatformBundle:Advert:edit.html.twig', array(
+            'advert' => $advert
+        ));
     }
 
     public function deleteAction($id)
